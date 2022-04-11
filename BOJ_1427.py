add4bde5a01b805c66afd83c0d4539d590747ad4
tt2too <tt2too@naver.com>
2022-04-11 19:48:21 +0900

    소트인사이드

---
+++ BOJ_1427.py
@@ -0,0 +1,10 @@
+# 1427 소트인사이드
+num = list(map(int, input()))
+count = 0
+for i in range(9, -1, -1):
+    for j in num:
+        if i == j:
+            print(j, end='')
+        
+    if count == len(num):
+        break
