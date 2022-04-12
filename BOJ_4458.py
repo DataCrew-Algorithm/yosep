c119f0753c048eb777cd809e0e5655a6ffa6af10
tt2too <tt2too@naver.com>
2022-04-12 22:01:39 +0900

    소트인사이드

--- BOJ_4458.py
+++ BOJ_4458.py
@@ -1,7 +1,11 @@
 # 4458 첫 글자를 대문자로
 
-N = int(input())
+N = int(input()) # 5
 
 for _ in range(N):
-    words = input()
-    print(words.replace(words[0], words[0].upper(), 1)) # 문자의 index 0번자리 문자를 대문자로 replace
+    words = input() # ex) 'powdered Toast Man'
+    print(words.replace(words[0], words[0].upper(), 1))
+    
+    # 문자의 index 0번자리 문자를 대문자로 replace
+    # 'test'replace(t, T)     >>> TesT
+    # 'test'replace(t, T, 1)  >>> Test
