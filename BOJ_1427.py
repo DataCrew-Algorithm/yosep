3aa31c144cae1413504e9e8e4ef7c464ac824ce0
tt2too <tt2too@naver.com>
2022-04-12 22:06:23 +0900

    소트인사이드

--- BOJ_1427.py
+++ BOJ_1427.py
@@ -1,6 +1,6 @@
 # 1427 소트인사이드
 num = list(map(int, input()))   # input N의 자리수를 순서대로 list에 저장
-for i in range(9, -1, -1):      # 9~0 큰수부터 순서대로 비교
+for i in range(9):              # 9~0 큰수부터 순서대로 비교
     for j in num:               # j는 각 자리수에 해당
         if i == j: 
             print(j, end='')    # 큰수부터 차례대로 출력
