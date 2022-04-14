69af8ae213dc19528742d42c773d6d130475fd61
tt2too <tt2too@naver.com>
2022-04-14 18:28:06 +0900

    저항

---
+++ BOJ_1076.py
@@ -0,0 +1,4 @@
+# 1076 저항
+
+reg_color = {'black':0, 'brown':1, 'red':2, 'orange':3, 'yellow':4, 'green':5, 'blue':6, 'violet':7, 'grey':8, 'white':9}
+print((10*reg_color[input()]+reg_color[input()])*10**reg_color[input()])
