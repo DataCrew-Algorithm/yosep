55c020f1f970242f99de45321a0b8f802a6779c8
tt2too <tt2too@naver.com>
2022-04-28 17:57:55 +0900

    성적 통계

--- BOJ_5800.py
+++ BOJ_5800.py
@@ -1,20 +1,23 @@
 # 5800 성적 통계
 
-def near_max(list):
+# 리스트의 연속되는 두 수의 차 중, 가장 큰 수를 반환하는 함수
+def largest_gap(list):
     tmp = []
     for i in range(len(list)-1):
         tmp.append(list[i+1]-list[i])
     return max(tmp)
 
-for i in range(int(input())):
-    tmp = list(map(int, input().split()))[1:]
-    tmp.sort()
-    print(f'Class {i+1}\nMax {tmp[-1]}, Min {tmp[0]}, Largest gap {near_max(tmp)}')
+for i in range(int(input())): # 교실 수 만큼 반복
+    tmp = list(map(int, input().split()))[1:] # 학생 수(index[0])을 제외하여 리스트에 저장 [5 30 25 76 23 78] -> [30 25 76 23 78]
+    tmp.sort() # 점수 정렬
+    print(f'Class {i+1}\nMax {tmp[-1]}, Min {tmp[0]}, Largest gap {largest_gap(tmp)}')
 
+# ex) input
 # 2
 # 5 30 25 76 23 78
 # 6 25 50 70 99 70 90
 
+# ex) output
 # Class 1
 # Max 78, Min 23, Largest gap 46
 # Class 2
