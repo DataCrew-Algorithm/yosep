93b384cc6e8b826730338d9877e09ef0a26071b0
tt2too <46961685+tt2too@users.noreply.github.com>
2022-05-03 01:34:51 +0900

    Delete BOJ_1076.py

--- BOJ_1076.py
+++
@@ -1,4 +0,0 @@
-# 1076 저항
-
-res_color = {'black':0, 'brown':1, 'red':2, 'orange':3, 'yellow':4, 'green':5, 'blue':6, 'violet':7, 'grey':8, 'white':9}
-print((10*res_color[input()]+res_color[input()])*10**res_color[input()])
